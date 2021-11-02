b48cf29277d7737d5abf3610d28fed53b70b6ebe
Michael Borel <mdborel85@gmail.com>
2021-11-02 08:49:14 -0500

    Started ceasar_cipher.rb

---
+++ caesar_cipher.rb
@@ -0,0 +1,15 @@
+# Ruby Programming
+# Project: Caesar Cipher
+
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+test_string = "This iz"
+offset = 5
+
+test_string.each_char do |letter|
+  if letter == ' '
+    puts ' '
+  else
+    puts alphabet[alphabet.index(letter) + offset]
+  end
+end
